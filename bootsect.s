7ff9ca0d646edaec96fb88fc59fd93699fd416dd
Your Name <you@example.com>
2022-04-15 22:50:54 +0800

    boot initial

--- bootsect.s
+++ bootsect.s
@@ -1,33 +1,37 @@
+!
+!
+!
 .globl begtext,begdata,begbss,endtext,enddata,endbss
-
 .text
 begtext:
 .data
 begdata:
 .bss
 begbss:
-	BOOTSEG = 0x07C0
-	entry start
+.text	
+BOOTSEG = 0x07C0
+
+entry start
 start:
 	jmpi go, BOOTSEG
 go:	mov ax,cs
 	mov ds,ax
 	mov es,ax
 	mov [msg1 + 17], ah
-	mov cs,#20
+	mov cx,#20
 	mov dx,#0x1004
 	mov bx,#0x000c
 	mov bp,#msg1
 	mov ax,#0x1301
-	int 0x1004
+	int 0x10
 loop1:	jmp loop1
-msg1:	.ascii "Loading system ..."
+msg1:	.ascii "Kim system load..."
 	.byte 13,10
 .org 510
-.word 0xAA55
+	.word 0xAA55
 .text
-;; endtext:	
+endtext:	
 .data
-endtext:
+enddata:
 .bss
 endbss:	
