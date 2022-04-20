93536f6b3985ec4259377b05cec3581c2c7c9953
kim <altroman@yeah.com>
2022-04-20 22:10:37 +0800

    bootsect

--- bootsect.s
+++ bootsect.s
@@ -1,6 +1,18 @@
-!
-!
-!
+!unit is 16(bytes)
+SYSSIZE = 0x3000                        ! this version of Linux system is less then 192KB
+
+SETUPLEN = 4				! nr of setup-sectors
+BOOTSEG  = 0x07c0			! original address of boot-sector
+INITSEG  = 0x9000			! we move boot here - out of the way
+SETUPSEG = 0x9020			! setup starts here
+SYSSEG   = 0x1000			! system loaded at 0x10000 (65536).
+ENDSEG   = SYSSEG + SYSSIZE		! where to stop loading
+
+
+! ROOT_DEV:	0x000 - same type of floppy as boot.
+!		0x301 - first partition on first drive etc
+ROOT_DEV = 0x306
+
 .globl begtext,begdata,begbss,endtext,enddata,endbss
 .text
 begtext:
@@ -9,26 +21,71 @@ begdata:
 .bss
 begbss:
 .text	
-BOOTSEG = 0x07C0
-
 entry start
 start:
-	jmpi go, BOOTSEG
-go:	mov ax,cs
-	mov ds,ax
-	mov es,ax
-	mov [msg1 + 17], ah
-	mov cx,#20
-	mov dx,#0x1004
-	mov bx,#0x000c
-	mov bp,#msg1
-	mov ax,#0x1301
-	int 0x10
-loop1:	jmp loop1
-msg1:	.ascii "Kim system load..."
+	mov ax, #BOOTSEG
+	mov ds, ax
+	mov ax, #INITSEG
+	mov es, ax
+	mov cx, #256
+	sub si,si
+	sub di,di
+	rep
+	movw
+	jmpi go,INITSEG
+go:	mov	ax,cs
+	mov	ds,ax
+	mov	es,ax
+! put stack at 0x9ff00.
+	mov	ss,ax
+	mov	sp,#0xFF00		! arbitrary value >>512
+
+! load the setup-sectors directly after the bootblock.
+! Note that 'es' is already set up.
+
+load_setup:
+	mov	dx,#0x0000		! drive 0, head 0
+	mov	cx,#0x0002		! sector 2, track 0
+	mov	bx,#0x0200		! address = 512, in INITSEG
+	mov	ax,#0x0200+SETUPLEN	! service 2, nr of sectors
+	int	0x13			! read it
+	jnc	ok_load_setup		! ok - continue
+	mov	dx,#0x0000
+	mov	ax,#0x0000		! reset the diskette
+	int	0x13
+	j	load_setup
+
+ok_load_setup:
+! Get disk drive parameters, specifically nr of sectors/track
+	mov	dl,#0x00
+	mov	ax,#0x0800		! AH=8 is get drive parameters
+	int	0x13
+	mov	ch,#0x00
+	seg cs                          ! mov cs:[sectors],ax
+	mov	sectors,cx
+	mov	ax,#INITSEG
+	mov	es,ax
+
+! Print some inane message
+
+	mov	ah,#0x03		! read cursor pos
+	xor	bh,bh
+	int	0x10
+	
+	mov	cx,#24
+	mov	bx,#0x0007		! page 0, attribute 7 (normal)
+	mov	bp,#msg1
+	mov	ax,#0x1301		! write string, move cursor
+	int	0x10
+
+sectors:
+	.word 0
+
+msg1:
 	.byte 13,10
-.org 510
-	.word 0xAA55
+	.ascii "KimOS Loading  ..."
+	.byte 13,10,13,10
+
 .text
 endtext:	
 .data
