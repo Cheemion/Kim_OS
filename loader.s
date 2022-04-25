eefd9644e014ad5f61490627d4469c137f74eb9b
kim <altroman@yeah.com>
2022-04-25 17:54:39 +0800

    fix bugs

--- loader.s
+++ loader.s
@@ -9,14 +9,15 @@
 
 .section .text
 .extern kernelMain
+.extern callConstructors
 .global loader
 
 loader:
 	mov $kernel_stack, %esp
+	call callConstructors
 	push %eax
 	push %ebx
 	call kernelMain
-
 _stop:
 	cli
 	hlt
