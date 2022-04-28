2c5602f144fdd146b3ba75b79ff490f1e557bac7
kim <altroman@yeah.com>
2022-04-28 18:15:59 +0800

    ignore release keyborad interrupt

--- keyboard.cpp
+++ keyboard.cpp
@@ -21,12 +21,21 @@ KeyboardDriver::~KeyboardDriver() {}
 
 void printf(char *);
 
-uint32_t KeyboardDriver::HandleInterrupt(uint32_t esp){
+uint32_t KeyboardDriver::HandleInterrupt(uint32_t esp) {
     uint8_t key = dataport.Read();
-    char* foo = "KEYBOARD 0x00";
-    char* hex = "0123456789ABCDEF";
-    foo[11] = hex[(key >> 4) & 0x0F];
-    foo[12] = hex[key & 0x0F];
-    printf(foo);
+    if(key < 0x80) { // only focus on the pressing
+	//when pressing the key, we got a interrupt with the highest bit being the zero
+	//when reselasing the key, we got a interrupt with the least bit being the one
+	switch(key) {
+	case 0xFA: break;
+	case 0x45: case 0xC5: break;
+	    default:
+		char* foo = "KEYBOARD 0x00";
+		char* hex = "0123456789ABCDEF";
+		foo[11] = hex[(key >> 4) & 0x0F];
+		foo[12] = hex[key & 0x0F];
+		printf(foo);
+	}
+    }
     return esp;
 }
