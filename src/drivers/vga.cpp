58f277d52ce207ebc985b284720257a7c0c426d4
kim <altroman@yeah.com>
2022-05-09 15:30:17 +0800

    minor bugs

--- src/drivers/vga.cpp
+++ src/drivers/vga.cpp
@@ -2,40 +2,48 @@
 using namespace myos::drivers;
 using namespace myos::common;
 void VideoGraphicsArray::WriteResigers(uint8_t* registers){
-    
+        //  misc
     miscPort.Write(*(registers++));
-
-    for(uint8_t i = 0; i < 5; i++){
-	sequenceIndexPort.Write(i);
-	sequenceDataPort.Write(*(registers++));
+    
+    // sequencer
+    for(uint8_t i = 0; i < 5; i++)
+    {
+        sequenceIndexPort.Write(i);
+        sequenceDataPort.Write(*(registers++));
     }
-
+    
+    // cathode ray tube controller
     crtcIndexPort.Write(0x03);
     crtcDataPort.Write(crtcDataPort.Read() | 0x80);
     crtcIndexPort.Write(0x11);
     crtcDataPort.Write(crtcDataPort.Read() & ~0x80);
-
+    
     registers[0x03] = registers[0x03] | 0x80;
     registers[0x11] = registers[0x11] & ~0x80;
     
-    for(uint8_t i = 0; i < 25; i++){
-	crtcIndexPort.Write(i);
-	crtcDataPort.Write(*(registers++));
+    for(uint8_t i = 0; i < 25; i++)
+    {
+        crtcIndexPort.Write(i);
+        crtcDataPort.Write(*(registers++));
     }
-
-    for(uint8_t i = 0; i < 9; i++){
-	graphicsControllerIndexPort.Write(i);
-	graphicsControllerDataPort.Write(*(registers++));
+    
+    // graphics controller
+    for(uint8_t i = 0; i < 9; i++)
+    {
+        graphicsControllerIndexPort.Write(i);
+        graphicsControllerDataPort.Write(*(registers++));
     }
-
-    for(uint8_t i = 0; i < 21; i++){
-	attributeControllerResetPort.Read();
-	attributeControllerIndexPort.Write(i);
-	attributeControllerWritePort.Write(*(registers++));
+    
+    // attribute controller
+    for(uint8_t i = 0; i < 21; i++)
+    {
+        attributeControllerResetPort.Read();
+        attributeControllerIndexPort.Write(i);
+        attributeControllerWritePort.Write(*(registers++));
     }
+    
     attributeControllerResetPort.Read();
     attributeControllerIndexPort.Write(0x20);
-    
 }
 uint8_t* VideoGraphicsArray::GetFrameBufferSegment() {
     graphicsControllerIndexPort.Write(0x06);
@@ -55,10 +63,13 @@ void VideoGraphicsArray::PutPixel(uint32_t x, uint32_t y, uint8_t colorIndex){
     *pixelAddress = colorIndex;
 }
 uint8_t VideoGraphicsArray::GetColorIndex(uint8_t r, uint8_t g, uint8_t b){
-    if(r == 0x00, g == 0x00, b == 0xA8)
+    if(r == 0x00 && g == 0x00 && b == 0xA8)
 	return 0x01;
 }
-VideoGraphicsArray::VideoGraphicsArray():
+
+
+            
+VideoGraphicsArray::VideoGraphicsArray() : 
     miscPort(0x3c2),
     crtcIndexPort(0x3d4),
     crtcDataPort(0x3d5),
@@ -70,8 +81,12 @@ VideoGraphicsArray::VideoGraphicsArray():
     attributeControllerReadPort(0x3c1),
     attributeControllerWritePort(0x3c0),
     attributeControllerResetPort(0x3da)
-{}    
-VideoGraphicsArray::~VideoGraphicsArray(){}
+{
+}
+
+
+VideoGraphicsArray::~VideoGraphicsArray() {}
+
 bool VideoGraphicsArray::SupportsMode(uint32_t widht, uint32_t height, uint32_t colordepth){
     return widht == 320 && height == 200 && colordepth == 8;
 }
@@ -86,7 +101,7 @@ bool VideoGraphicsArray::SetMode(uint32_t widht, uint32_t height, uint32_t color
 				    /* CRTC */
 					    0x5F, 0x4F, 0x50, 0x82, 0x54, 0x80, 0xBF, 0x1F,
 					    0x00, 0x41, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-					    0x9C, 0x0E, 0x8F, 0x28,	0x40, 0x96, 0xB9, 0xA3,
+					    0x9C, 0x0E, 0x8F, 0x28, 0x40, 0x96, 0xB9, 0xA3,
 					    0xFF,
 				    /* GC */
 					    0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0x05, 0x0F,
@@ -94,7 +109,7 @@ bool VideoGraphicsArray::SetMode(uint32_t widht, uint32_t height, uint32_t color
 				    /* AC */
 					    0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
 					    0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
-					    0x41, 0x00, 0x0F, 0x00,	0x00
+					    0x41, 0x00, 0x0F, 0x00, 0x00
 				    };
 
     WriteResigers(g_320x200x256);
