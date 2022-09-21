2ca00f74c5edd92183e41e7b610e7569af941105
kim <altroman@yeah.net>
2022-09-21 23:47:37 +0800

    memory management

--- src/memorymanagement.cpp
+++ src/memorymanagement.cpp
@@ -1,68 +1,74 @@
-#include "../include/memorymanagement.h"
+ 
+#include <memorymanagement.h>
 
 using namespace myos;
-namespace myos{
+using namespace myos::common;
 
-MemoryManager* MemoryManager::activeMemoryManager;
 
-MemoryManager::MemoryManager(common::size_t start, common::size_t size)
+MemoryManager* MemoryManager::activeMemoryManager = 0;
+        
+MemoryManager::MemoryManager(size_t start, size_t size)
 {
     activeMemoryManager = this;
+    
     if(size < sizeof(MemoryChunk))
     {
         first = 0;
-    } 
+    }
     else
     {
         first = (MemoryChunk*)start;
-        first->allocated = false;
-        first->prev = 0;
-        first->next = 0;
-        first->size = size - sizeof(MemoryChunk);
+        
+        first -> allocated = false;
+        first -> prev = 0;
+        first -> next = 0;
+        first -> size = size - sizeof(MemoryChunk);
     }
 }
 
 MemoryManager::~MemoryManager()
 {
-    if(activeMemoryManager == this) 
+    if(activeMemoryManager == this)
         activeMemoryManager = 0;
 }
-
-void* MemoryManager::malloc(common::size_t size)
+        
+void* MemoryManager::malloc(size_t size)
 {
-    MemoryChunk* result = 0;
+    MemoryChunk *result = 0;
+    
     for(MemoryChunk* chunk = first; chunk != 0 && result == 0; chunk = chunk->next)
-    {
         if(chunk->size > size && !chunk->allocated)
             result = chunk;
-    }
-
+        
     if(result == 0)
         return 0;
-
+    
     if(result->size >= size + sizeof(MemoryChunk) + 1)
     {
-        MemoryChunk* temp = (MemoryChunk*)((common::size_t)result + sizeof(MemoryChunk) + size);
+        MemoryChunk* temp = (MemoryChunk*)((size_t)result + sizeof(MemoryChunk) + size);
+        
         temp->allocated = false;
-        temp->size = result->size -size -sizeof(MemoryChunk);
+        temp->size = result->size - size - sizeof(MemoryChunk);
         temp->prev = result;
         temp->next = result->next;
         if(temp->next != 0)
             temp->next->prev = temp;
-
         
         result->size = size;
         result->next = temp;
     }
+    
     result->allocated = true;
-    return (void*)(((common::size_t)result) + sizeof(MemoryChunk));
+    return (void*)(((size_t)result) + sizeof(MemoryChunk));
 }
 
 void MemoryManager::free(void* ptr)
 {
-    MemoryChunk* chunk = (MemoryChunk*)((common::size_t)ptr - sizeof(MemoryChunk));
-    chunk->allocated = false;
-    if(chunk->prev != 0 &&!chunk->prev->allocated)
+    MemoryChunk* chunk = (MemoryChunk*)((size_t)ptr - sizeof(MemoryChunk));
+    
+    chunk -> allocated = false;
+    
+    if(chunk->prev != 0 && !chunk->prev->allocated)
     {
         chunk->prev->next = chunk->next;
         chunk->prev->size += chunk->size + sizeof(MemoryChunk);
@@ -71,14 +77,19 @@ void MemoryManager::free(void* ptr)
         
         chunk = chunk->prev;
     }
+    
     if(chunk->next != 0 && !chunk->next->allocated)
     {
         chunk->size += chunk->next->size + sizeof(MemoryChunk);
         chunk->next = chunk->next->next;
         if(chunk->next != 0)
-           chunk->next->prev = chunk;
+            chunk->next->prev = chunk;
     }
+    
 }
+
+
+
 
 void* operator new(unsigned size)
 {
@@ -114,6 +125,4 @@ void operator delete[](void* ptr)
 {
     if(myos::MemoryManager::activeMemoryManager != 0)
         myos::MemoryManager::activeMemoryManager->free(ptr);
-}
-
 }
